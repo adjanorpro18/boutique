02f23830af4748dba855940a97ca67c0b08e0f21
Enyon ADJANOR <enyonadjanor@gmail.com>
2021-07-15 12:43:23 +0200

    csrf-security

---
+++ src/main/java/com/example/boutique/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.boutique;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+
+                // URL autorizations:
+                .authorizeRequests()
+                // The order of the rules matters and the more specific rules should go first.
+                // ne pas oublier le / devant les URLs
+                .antMatchers("/", "/index.html").permitAll()
+                .anyRequest().authenticated()
+
+                // Authentication mode:
+                .and().formLogin()  //  redirect to /login HTML page and then to the resource after successfull authentication
+
+                //disable csrf protection
+                .and().csrf().disable()
+        ;
+    }
+}
